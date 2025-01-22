54853e22ae580858e3535d4f421bd559762ce0ff
Tony <ytyc2k@gmail.com>
2025-01-22 00:52:29 -0500

    格式调整

--- 递归.py
+++ 递归.py
@@ -1,7 +1,9 @@
-'''
+"""
 阶乘递归
 5*4*3*2*1=120
-'''
+"""
+
+
 def factorial(n):
     if n < 0:
         raise ValueError("n must be a non-negative integer")
@@ -9,19 +11,23 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
+
 # 示例
 number = 5
 print(f"{number}! = {factorial(number)}")
+
 
 def fs(n):
     global s
     if n < 0:
         raise ValueError("n must be a non-negative integer")
     if n < 2:
-        s+='1'
+        s += "1"
         return 1
-    s+=f'{n}*'
-    return n*fs(n-1)
-s=''
-rs=fs(5)
-print(f'{s}={rs}')
+    s += f"{n}*"
+    return n * fs(n - 1)
+
+
+s = ""
+rs = fs(6)
+print(f"{s}={rs}")
